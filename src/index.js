ef7045d86e32d0e70bf73bbe49f2b3b18b35f521
Evelina <krugerevelina@gmail.com>
2023-10-29 13:52:54 +0100

    BrowserRouter added

--- src/index.js
+++ src/index.js
@@ -3,10 +3,19 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
+import Home from './views/Home';
+import Contacts from './views/Contacts';
+import NotFound from './views/NotFound';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element= {<Home/>} />
+      <Route path='/contacts' element= {<Contacts/>} />
+      <Route path='*' element= {<NotFound/>} />
+    </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
