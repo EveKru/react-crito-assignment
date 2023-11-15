c1cfbcf1d6103a35455813977a352cfa3c191939
Evelina <krugerevelina@gmail.com>
2023-11-15 19:35:44 +0100

    Messageform almost completed

--- src/components/Messageform.js
+++ src/components/Messageform.js
@@ -4,14 +4,49 @@ const Messageform = () => {
 
     const [name, setName] = useState('')
     const [email, setEmail] = useState('')
-    const [message, setMessage] =useState('')
+    const [message, setMessage] = useState('')
+    const [error, setError] = useState('')
+    const [success, setSuccess] = useState ('')
 
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        setError('')
+        setSuccess('')
 
+        const user = {name, email, message}
+        const json = JSON.stringify(user)
+
+        const result = await fetch('https://win23-assignment.azurewebsites.net/api/contactform', {
+            method: 'post',
+            headers: {
+                'content-type':'application/json'
+            },
+            body: json
+        })
+
+        switch (result.status) {
+            case 200:
+                setSuccess('Meddelandet har skickats.')
+                clearForm()
+            break
+            case 400:
+                setError('Något gick fel.')
+            break
+        }
+    }
+
+    const clearForm = () => {
+        setName('')
+        setEmail('')
+        setMessage('')
+    }
 
   return (
-    <form className='registrationform'>
+    <form className='registrationform' onSubmit={handleSubmit} noValidate>
         <div className='container'>
         <h1>Leave us a message <br/> for any information.</h1>
+        <p className='errorcolor'>{error} </p>
+        <p className='successcolor'>{success}</p>
         <div className='input-name'>
             <input placeholder='Name*' type='text' value={name} onChange={(e) => setName(e.target.value)}/>
         </div>
